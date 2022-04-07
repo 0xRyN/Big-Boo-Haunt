38ae21d5caa82bb86a800c048f2e0129b427172e
Rayan Al Azawi <ryanalaz11@gmail.com>
2022-04-07 22:39:58 +0200

    Added C Makefile, architecture

---
+++ server/include/server.h
@@ -0,0 +1,17 @@
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#endif
